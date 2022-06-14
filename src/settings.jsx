ced8a8ea21d83632e1a8c5cc8bfd2ddc0186d80f
Rabee604 <abla.rabee@hotmail.com>
2022-06-14 12:58:36 +0200

    With URL

--- src/settings.jsx
+++ src/settings.jsx
@@ -1,4 +1,4 @@
 // const URL = "https://peternedergaard.xyz/CA2backend";
-const URL = "http://localhost:8080/CA2backend_war_exploded";
+const URL = "http://localhost:8080/sem3ExamBackend_war_exploded";
 
 export default URL;
